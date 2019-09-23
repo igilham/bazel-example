908290cfc8409892a00068192871b16ce2269334
Ian Gilham <ian.gilham@gmail.com>
2019-09-23 11:31:13 +0100

    add module names to typescript libs

--- libs/tslib003/index.ts
+++ libs/tslib003/index.ts
@@ -1,4 +1,4 @@
 // must use absolute workspace path to make imports work
-import tslib001 from 'bazel_example/libs/tslib001'
+import tslib001 from '@igilham/tslib001'
 
 export default () => `I talk to tslib001: ${tslib001()}`
